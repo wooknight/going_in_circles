970f3d029eae8506166ef901f9d1ad3763d8e43f
Ramesh Naidu <ramesh@bstocksolutions.com>
2020-06-22 19:56:57 -0700

    Successfully listed buckets

--- Go/Go in Practice/UploadToCloud/Upload.go
+++ Go/Go in Practice/UploadToCloud/Upload.go
@@ -10,16 +10,18 @@ import (
 
 	"cloud.google.com/go/storage"
 	"github.com/rs/zerolog/log"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
-var BACKUP_BUCKET = "mts-ramesh-sandbox-1"
+var backupBucket = "mts-ramesh-sandbox"
+var mtsBackupAllFiles *storage.BucketHandle
 
 func main() {
 	//create our backup bucket . this is where we will save all our files , inbound and outbound
 
 	// credentials in env comes in escaped, need to unescape
-	creds, err := ioutil.ReadFile("ramesh-sandbox-755a791f557a.json")
+	creds, err := ioutil.ReadFile("ramesh-sandbox-81dfd482f50f.json")
 	if err != nil {
 		log.Printf("Failed to ioen cert %v", err)
 		return
@@ -33,14 +35,38 @@ func main() {
 		log.Printf("GCP UploadLocalToRemote failed . client not created\t%v", err)
 		return
 	}
+	projectID := "ramesh-sandbox"
 
-	if err := mtsBackup.Bucket(BACKUP_BUCKET).Create(ctx, "ramesh-sandbox", nil); err != nil {
-		log.Printf("could not create bucket %v", err)
-		return
+	// if err := mtsBackup.Bucket(BACKUP_BUCKET).Create(ctx, "ramesh-sandbox", nil); err != nil {
+	// 	log.Printf("could not create bucket %v", err)
+	// 	return
 
-		// TODO: handle error.
+	// 	// TODO: handle error.
+	// }
+	it := mtsBackup.Buckets(ctx, projectID)
+	log.Debug().Msgf("Buckets %v", it)
+	for {
+		battrs, err := it.Next()
+		if err == iterator.Done {
+			//we did not find the bucket so create it
+			if err := mtsBackup.Bucket(backupBucket).Create(ctx, projectID, nil); err != nil {
+				log.Error().Err(err).Msgf("could not create bucket %s for project %s", backupBucket, projectID)
+				return
+			}
+			mtsBackupAllFiles = mtsBackup.Bucket(backupBucket) //bucket handle
+			break
+		}
+		if err != nil {
+			log.Error().Err(err).Msgf("Error while looping for buckets in project %s", projectID)
+			return
+		}
+		if battrs.Name == backupBucket {
+			mtsBackupAllFiles = mtsBackup.Bucket(backupBucket) //bucket handle
+			log.Debug().Msgf("bucket %s found for project %s", backupBucket, projectID)
+			break
+		}
 	}
-	mtsBackupAllFiles := mtsBackup.Bucket(BACKUP_BUCKET) //bucket handle
+	mtsBackupAllFiles := mtsBackup.Bucket(backupBucket) //bucket handle
 	l := "randi"
 	f, err := os.Open(l)
 	if err != nil {
