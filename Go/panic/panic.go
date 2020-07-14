19316e507984ce7cf9e790800020682afeac7990
Ramesh Naidu <ramesh@bstocksolutions.com>
2020-07-14 13:06:19 -0700

    multiple defers

--- Go/panic/panic.go
+++ Go/panic/panic.go
@@ -6,12 +6,20 @@ import (
 
 func main() {
 	defer func() {
-		fmt.Println("Inside my defer")
-		if r := recover(); r != nil && r == "Bhosada lag hgaya" {
+		fmt.Println("Inside main defer")
+		if r := recover(); r != nil && r == "Bhosada lag gaya" {
 			fmt.Println("I love pussy ")
 		}
 	}()
+	fmt.Println("Inside main and getting ready to call bho")
+	bho()
+}
+
+func bho() {
+	defer func() {
+		fmt.Println("Inside bho defer\nI love bhosada my dear")
+	}()
+
 	fmt.Println("Getting ready to do a panic")
 	panic("Bhosada lag hgaya")
-
 }
