a2bfb70762a27a828becc1594a0782f75fb5faec
Ramesh Naidu <ramesh@bstocksolutions.com>
2020-08-10 21:55:54 -0700

    rest endpoints done

--- Go/tinkr/routers.go
+++ Go/tinkr/routers.go
@@ -1,13 +1,42 @@
-import routers
+package main
 
 import (
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
+const FQDN = "fqdn"
+const CATEGORY = "category"
+const BUSINESS = "business"
+const RATING = "rating"
 
-func routers () {
+func main() {
 
-	catRouter := new httpRouter()
-	businessRouter := new httpRouter()
-	ratingRouter := new httpRouter()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/"+CATEGORY, categoryHandler)
+	r.HandleFunc("/"+BUSINESS, businessHandler)
+	r.HandleFunc("/"+RATING, ratingsHandler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:9000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+func categoryHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Category"))
+}
+
+func businessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Business"))
+}
+
+func ratingsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Rating"))
 }
