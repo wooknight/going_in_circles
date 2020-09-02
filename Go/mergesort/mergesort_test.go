e3ae7f373c0c26a53f278b00e1ac05daf8b461fb
Ramesh Naidu <rameshnaidu@gmail.com>
2020-09-01 17:50:37 -0700

    sugar

--- Go/mergesort/mergesort_test.go
+++ Go/mergesort/mergesort_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	
 	"reflect"
 	"testing"
 )
@@ -13,11 +12,11 @@ func TestMergesort(t *testing.T) {
 		input2 []int
 		result []int
 	}{
-		{input1:[]int{1, 3, 2}, input2:[]int{4, 5, 6},result:[]int {1, 2, 3, 4, 5, 6}},
+		{input1: []int{1, 3, 2}, input2: []int{4, 5, 6}, result: []int{1, 2, 3, 4, 5, 6}},
 	}
 	for _, tt := range mergeTests {
 		result := mergesort(tt.input1, tt.input2)
-		if reflect.DeepEqual(result , tt.result) {
+		if reflect.DeepEqual(result, tt.result) {
 			t.Errorf("Result not matching . Need %v . Got %v", tt.result, result)
 		}
 	}
