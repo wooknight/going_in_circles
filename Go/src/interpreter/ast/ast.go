f97a53521cd0736471b273f980c5b6c9378f5836
Your Name <rameshnaidu@gmail.com>
2020-09-27 17:28:20 -0700

    Function to string

--- Go/src/interpreter/ast/ast.go
+++ Go/src/interpreter/ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"interpreter/token"
+	"strings"
 )
 
 type Node interface {
@@ -119,6 +120,32 @@ func (bl *BooleanLiteral) TokenLiteral() string { return bl.Token.Literal }
 func (bl *BooleanLiteral) String() string       { return bl.Token.Literal }
 
 
+
+type FunctionLiteral struct {
+	Token token.Token
+	Parameters [] *Identifier
+
+	Body *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string       {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _,p := range fl.Parameters{
+		params=append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params,", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+	return out.String()
+}
+
+
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64
@@ -177,4 +204,51 @@ func (b *Boolean) TokenLiteral() string {
 }
 func (b *Boolean) String() string {
 	return b.Token.Literal
+}
+
+type IfExpression struct{
+	Token token.Token
+	Condition Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+
+
+func (i *IfExpression) expressionNode(){}
+func (i *IfExpression) TokenLiteral() string {
+	return i.Token.Literal
+}
+func (i *IfExpression) String() string {
+	var out bytes.Buffer
+		out.WriteString("if")
+		out.WriteString(i.Condition.String())
+		out.WriteString(" ")
+		out.WriteString(i.Consequence.String())
+	if i.Alternative != nil {
+		out.WriteString(" else ")
+		out.WriteString(i.Alternative.String())
+	}
+	return out.String()
+}
+
+
+type BlockStatement struct {
+	Token token.Token
+	Statements []Statement
+
+}
+
+
+func (bs *BlockStatement) expressionNode(){}
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+func (bs *BlockStatement) String() string {
+
+	var out bytes.Buffer
+	for _,s := range bs.Statements{
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
