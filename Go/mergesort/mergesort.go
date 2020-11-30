9f596c3f7e002dda6127b37ba6bb249d6439866a
Your Name <rameshnaidu@gmail.com>
2020-11-29 18:01:31 -0800

    Updated Mergesort to made from scratch

--- Go/mergesort/mergesort.go
+++ Go/mergesort/mergesort.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/pkg/profile"
 )
@@ -12,38 +11,53 @@ func main() {
 	fmt.Println("hello world")
 }
 
-func mergesort(arr1, arr2 []int) (sortedarr []int) {
+func mergesort(arr1, arr2 []int) []int {
 	if len(arr1) == 0 {
 		return arr2
-	}
-	if len(arr2) == 0 {
+	} else if len(arr2) == 0 {
 		return arr1
 	}
-	if len(arr1) == 1 {
-		arr2 = append(arr2, arr1[0])
-		sort.Ints(arr2)
-		return arr2
+	if len(arr1) <= 3 && len(arr2) <= 3 {
+		return merge(arr1, arr2)
+	}
+	arr1 = mergesort(arr1[:len(arr1)/2], arr1[len(arr1)/2+1:])
+	arr2 = mergesort(arr2[:len(arr2)/2], arr2[len(arr2)/2+1:])
+	return merge(arr1, arr2)
+}
 
+func merge(arr1, arr2 []int) []int {
+	totLen := len(arr1) + len(arr2)
+	sortedArr := make([]int, totLen)
+	i := 0
+	j := 0
+	for k := 0; k < totLen; k++ {
+		if i < len(arr1) && j < len(arr2){
+			if arr1[i]<arr2[j]{
+				sortedArr[k]=arr1[i]
+				i++
+			}else{
+				sortedArr[k]=arr2[j]
+				j++
+			}
+		}else if i < len(arr1) {
+			sortedArr[k] = arr1[i]
+			i++
+		} else if j < len(arr2) {
+				sortedArr[k]=arr2[j]
+				j++
+			}
 	}
-	if len(arr2) == 1 {
-		arr1 = append(arr1, arr2[0])
-		sort.Ints(arr1)
-		return arr1
-	}
-	return mergesort(
-		mergesort(arr1[:len(arr1)/2], arr1[len(arr1)/2+1:]),
-		mergesort(arr2[:len(arr2)/2], arr2[len(arr2)/2+1:]),
-	)
+	return sortedArr
 }
 
 func insertionSort(arr []int) []int {
-for key, val := range arr{
-	subarr:= arr[0:len(arr)-1]
-	for key1,val1 := range subarr{
-		if val > val1 {
-			arr[key] , subarr[key1]= val1,val
+	for key, val := range arr {
+		subarr := arr[0 : len(arr)-1]
+		for key1, val1 := range subarr {
+			if val > val1 {
+				arr[key], subarr[key1] = val1, val
+			}
 		}
 	}
-}
-return arr
+	return arr
 }
