749c105402d8eb65e6c8986d85c357364f9408fa
Your Name <rameshnaidu@gmail.com>
2020-11-29 21:20:05 -0800

    Implemented quicksort

---
+++ Go/quicksort/quicksort.go
@@ -0,0 +1,28 @@
+package main
+
+func quicksort(arr []int,lower , upper int){
+	if (upper - lower) > 0 {
+	pvt:=partition(arr,len(arr)-1)//last element
+	if pvt > 0 {
+		quicksort(arr[:pvt-1],0,pvt-1)
+	}
+	if pvt < len(arr)-1 {
+		quicksort(arr[:pvt+1],pvt+1,len(arr)-1)
+	}
+}
+
+}
+
+func partition(arr []int,idx int) int {
+	pvt := 0
+
+	for i:= range arr{
+		if arr[i] < arr[idx]{
+			arr[i],arr[pvt]=arr[pvt],arr[i]
+			pvt++
+		}
+	}
+	arr[idx],arr[pvt]=arr[pvt],arr[idx]
+	return pvt
+}
+
