dc228802a3f2824cfe7016b0fba033bd4ceb481c
Ramesh Naidu <ramesh@bstocksolutions.com>
2020-12-02 11:10:23 -0800

    resolved quicksort

--- Go/quicksort/quicksort.go
+++ Go/quicksort/quicksort.go
@@ -1,29 +1,50 @@
 package main
 
-// import "fmt"
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
 
-func quicksort(arr []int,lower , upper int){
-//	fmt.Printf("Inside quicksort - %v\n",arr)
-	if (upper - lower) > 0 {
-	pvt:=partition(arr,len(arr)-1)//last element
-	if pvt > 0 {
-		quicksort(arr[:pvt-1],0,pvt-1)
+func printStack() {
+	var buf [8192]byte
+	n := runtime.Stack(buf[:], false)
+	os.Stdout.Write(buf[:n])
+}
+
+func quicksort(arr []int) {
+	defer func() {
+		fmt.Println("Inside quicksort defer")
+
+		if r := recover(); r != nil {
+			fmt.Printf("Arr value is %v \n%v", arr, r)
+		}
+	}()
+
+	if (len(arr) - 1) > 0 {
+		pvt := partition(arr) //last element
+		if pvt > 0 {
+			quicksort(arr[:pvt])
+		}
+		if pvt < (len(arr) - 1) {
+			quicksort(arr[:pvt+1])
+		}
 	}
-		quicksort(arr[:pvt+1],pvt+1,len(arr)-1)
 }
 
-}
-
-func partition(arr []int,idx int) int {
+func partition(arr []int) int {
 	pvt := 0
-
-	for i:= range arr{
-		if arr[i] < arr[idx]{
-			arr[i],arr[pvt]=arr[pvt],arr[i]
+	idx := len(arr) - 1
+	for i := range arr {
+		if arr[i] < arr[idx] {
+			arr[i], arr[pvt] = arr[pvt], arr[i]
 			pvt++
 		}
 	}
-	arr[idx],arr[pvt]=arr[pvt],arr[idx]
+	arr[idx], arr[pvt] = arr[pvt], arr[idx]
+
+	if idx != pvt {
+		fmt.Printf("Swapped %d in %d to %d in %d\n\n\n%v\n\n", arr[idx], idx, arr[pvt], pvt, arr)
+	}
 	return pvt
 }
-
