bad630cf67c976c7313f802d7bc041bff862556f
Ramesh Naidu <ramesh@bstocksolutions.com>
2021-04-04 08:16:49 -0700

    Generateed all permutations

--- Go/tree/tree.go
+++ Go/tree/tree.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //binary search tree
@@ -13,7 +15,9 @@ type tree struct {
 }
 
 func main() {
-	fmt.Printf("Printing treee")
+
+	// printAllRepeats(7)
+	printAllNonRepeats()
 
 }
 
@@ -52,12 +56,50 @@ func searchRecursive(root *tree, target int) (*tree, error) {
 		//left tree
 		return searchRecursive(curr.left, target)
 	}
+	return nil, nil
 }
 
-func insert(root *treeval *tree){
+// func insert(root *treeval *tree){
 
+// }
+
+// func hashTable (){
+
+// }
+
+func printAllRepeats(num int) {
+	phelper("", num)
 }
 
-func hashTable (){
-	
+func phelper(slate string, num int) {
+	if num == 0 {
+		fmt.Println(slate)
+		return
+	}
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for _, val := range arr {
+		phelper(slate+strconv.Itoa(val), num-1)
+	}
 }
+
+func printAllNonRepeats() {
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	pNRHelper("", arr)
+}
+
+func pNRHelper(slate string, bag []int) {
+	if len(bag) == 0 {
+		fmt.Println(slate)
+		return
+	}
+	for _, val := range bag {
+		stackBag := make([]int,0)
+		//add 0-9 in StackBag if not val and not in slate
+		for i:=0;i<=9;i++{
+			if val!=i && !strings.ContainsRune(slate, (rune)(i) + '0'){
+				stackBag = append(stackBag,i)
+			}  
+		}
+		pNRHelper(slate+strconv.Itoa(val), stackBag)
+		}
+	}
