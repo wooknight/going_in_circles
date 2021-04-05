a671c2a46a7d61d27c2c6e86cc6310b59e501ea5
Ramesh Naidu <ramesh@bstocksolutions.com>
2021-04-05 11:41:26 -0700

    Combinations

--- Go/tree/tree.go
+++ Go/tree/tree.go
@@ -17,8 +17,8 @@ type tree struct {
 func main() {
 
 	// printAllRepeats(7)
-	printAllNonRepeats()
-
+	// printAllNonRepeats(2)
+	printAllSubsets(5)
 }
 
 func searchIterative(root *tree, target int) (*tree, error) {
@@ -82,24 +82,44 @@ func phelper(slate string, num int) {
 	}
 }
 
-func printAllNonRepeats() {
+func printAllNonRepeats(num int) {
 	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	pNRHelper("", arr)
+	pNRHelper("", arr, num)
 }
 
-func pNRHelper(slate string, bag []int) {
-	if len(bag) == 0 {
+func pNRHelper(slate string, bag []int, num int) {
+	if len(bag) == 0 || len(slate) == num {
 		fmt.Println(slate)
 		return
 	}
 	for _, val := range bag {
-		stackBag := make([]int,0)
+		stackBag := make([]int, 0)
 		//add 0-9 in StackBag if not val and not in slate
-		for i:=0;i<=9;i++{
-			if val!=i && !strings.ContainsRune(slate, (rune)(i) + '0'){
-				stackBag = append(stackBag,i)
-			}  
+		for i := 0; i <= 9; i++ {
+			if val != i && !strings.ContainsRune(slate, (rune)(i)+'0') {
+				stackBag = append(stackBag, i)
+			}
 		}
-		pNRHelper(slate+strconv.Itoa(val), stackBag)
-		}
+		pNRHelper(slate+strconv.Itoa(val), stackBag, num)
 	}
+}
+
+func printAllSubsets(num int) {
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	pSubsetsHelper([]int{}, arr, num)
+}
+
+func pSubsetsHelper(slate []int, bag []int, num int) {
+	// fmt.Println(bag, slate)
+	if len(bag) == 0 || len(slate) == num {
+		fmt.Println(slate)
+		return
+	}
+	//exclude case
+	pSubsetsHelper(slate, bag[1:], num)
+	//include case
+	// newSlate := append(slate[:0:0], slate...)
+	// newSlate = append(newSlate, bag[0])
+	// pSubsetsHelper(newSlate, bag[1:], num)
+	pSubsetsHelper(append(slate, bag[0]), bag[1:], num)
+}
