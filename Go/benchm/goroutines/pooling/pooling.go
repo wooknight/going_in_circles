769ecad1738682576026e02d77676a9fed0fd13a
Ramesh Naidu <ramesh@bstocksolutions.com>
2021-07-04 11:23:10 -0700

    pooling goroutines

--- Go/benchm/goroutines/pooling/pooling.go
+++ Go/benchm/goroutines/pooling/pooling.go
@@ -2,42 +2,54 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"strconv"
 	"sync"
 )
 
-func cook(recv chan string, wg *sync.WaitGroup) {
+func cook(recv chan string, wg *sync.WaitGroup, num int) {
 	// fmt.Println("Inside goroutine")
 	defer wg.Done()
 	data, ok := <-recv
 	if ok {
 		fmt.Println(data)
 	} else {
-		fmt.Println("Closed")
+		fmt.Println("Closed" + strconv.Itoa(num))
 	}
 }
 
-// func pool() {
-// 	sem := 0
-// 	numCpu := runtime.NumCPU()
-// 	for i := 0; i < numCpu; i++ {
-// 		sem++
-// 		go cook("Sending on CPU " + string(i))
-// 	}
-// }
-
 func main() {
 	var wg sync.WaitGroup
-	comm := make(chan string, 1)
+	comm := make(chan string)
 	// fmt.Println("Inside main")
-	comm <- "I love Scooty"
 	wg.Add(1001)
 	// fmt.Println("Inside main")
-	go cook(comm, &wg)
+	go cook(comm, &wg, 0)
 	// time.Sleep(1000)
-	comm <- "Done with Bluesy"
+	comm <- "I love Scooty"
 	for i := 0; i < 1000; i++ {
-		go cook(comm, &wg)
+		go cook(comm, &wg, i)
+		if i < 700 {
+			comm <- "Done with " + strconv.Itoa(i)
+		}
 	}
 	close(comm)
+	wg.Wait()
+	fmt.Println("Starting anew")
+	ch := make(chan string)
+	numCpu := runtime.NumCPU()
+	wg.Add(numCpu)
+	for i := 0; i < numCpu; i++ {
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			fmt.Println("Inside pool goroutine")
+			for p := range ch {
+				fmt.Println("Got data ", p)
+			}
+			fmt.Println("Shutting down")
+		}(&wg)
+	}
+	ch <- "Done with " + strconv.Itoa(numCpu)
+	close(ch)
 	wg.Wait()
 }
