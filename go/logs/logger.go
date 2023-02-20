c4ee2e3f92322793b5d3426e7ebaec3c765a2c8a
Ramesh Naidu <ramesh@bstock.com>
2023-02-19 18:32:22 -0800

    Add all

--- go/logs/logger.go
+++ go/logs/logger.go
@@ -1,87 +1,21 @@
 package caching
 
-import "fmt"
-
-// "time"
-// "crypto/sha1"
-
-const (
-	rows = 4 * 1024
-	cols = 4 * 1024
+import (
+	"bufio"
+	"fmt"
+	"os"
 )
 
-var matrix [rows][cols]byte
-
-type data struct {
-	v byte
-	p *data
-}
-
-var list *data
-
-func init() {
-	var last *data
-	for row := 0; row < rows; row++ {
-		for col := 0; col < cols; col++ {
-			var d data
-			if list == nil {
-				list = &d
-			}
-			if last != nil {
-				last.p = &d
-			}
-			last = &d
-			if row%2 == 0 {
-				matrix[row][col] = 0xFF
-				d.v = 0xFF
-			}
+func main() {
+	f, _ := os.Open("bids.log")
+	reader := bufio.NewReader(f)
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Exiting")
+			break
 		}
+		fmt.Println(str)
 	}
-
-	// Count the number of elements in the link list.
-	var ctr int
-	d := list
-	for d != nil {
-		ctr++
-		d = d.p
-	}
-
-	fmt.Println("Elements in the link list", ctr)
-	fmt.Println("Elements in the matrix", rows*cols)
-}
-
-func RowTraverse() int {
-	ctr := 0
-	for row := 0; row < rows; row++ {
-		for col := 0; col < cols; col++ {
-			if matrix[row][col] == 0xFF {
-				ctr++
-			}
-		}
-	}
-	return ctr
-}
-
-func ColTraverse() int {
-	ctr := 0
-	for col := 0; col < cols; col++ {
-		for row := 0; row < rows; row++ {
-			if matrix[row][col] == 0xFF {
-				ctr++
-			}
-		}
-	}
-	return ctr
-}
-
-func LinkedListTraverse() int {
-	var ctr int
-	d := list
-	for d != nil {
-		if d.v == 0xFF {
-			ctr++
-		}
-		d = d.p
-	}
-	return ctr
+	f.Close()
 }
---
+++ go/trevor_sawler/go-micro/go.mod
@@ -0,0 +1,3 @@
+module github.com/wooknight/going_in_circles/go/trevor_sawler/go-micro
+
+go 1.19
