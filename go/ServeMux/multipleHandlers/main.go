5496a613df0a1828f519ade8be1ed90abb220dee
Ramesh Naidu <rameshnaidu@gmail.com>
2025-02-21 09:06:06 -0800

    Added one more rule

--- go/ServeMux/multipleHandlers/main.go
+++ go/ServeMux/multipleHandlers/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	_ "net/http/pprof"
 	"time"
@@ -18,19 +18,17 @@ func (p *UUID) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		b := make([]byte, c)
 		_, err := rand.Read(b)
 		if err != nil {
-			log.Fatal("Error %v", err)
+			log.Fatalf("Error %v", err)
 
 		}
-		fmt.Fprintf(w, fmt.Sprintf("%x", b))
+		fmt.Fprintf(w, "%s", fmt.Sprintf("%x", b))
 		return
 	}
 	http.NotFound(w, r)
-	return
 }
-bobcatminer				172.18.1.148	fe80:0000:0000:0000:ea78:29ff:fe54:e95e	E8:78:29:54:E9:5E
+
 func main() {
 	newMux := http.NewServeMux()
-	newMux.PathPre
 	newMux.HandleFunc("/randomFloat", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, rand.Float64())
 	})
